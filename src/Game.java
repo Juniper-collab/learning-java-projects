dc4adaa010271f8823595626cbef26ca1cd8a782
Juniper-collab <bebemikate@gmail.com>
2026-02-19 17:17:37 +0100

    Working guessing Game

---
+++ src/Game.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    private int secretNumber;
+    private Scanner scanner;
+
+    public Game() {
+        Random random = new Random();
+        secretNumber = random.nextInt(100) +1;
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("Willkommen zum Zahlenratespiel! Kannst du das Spiel gewinnen?");
+        System.out.println("Rate eine Zahl zwischen 1 und 100.");
+
+
+        int guess = 0;
+
+        while (guess != secretNumber){
+            System.out.println("An welche Zahl denkst du?: ");
+            guess = scanner.nextInt();
+
+            if (guess < secretNumber){
+                System.out.println("Zu klein!");
+            } else if (guess > secretNumber){
+                System.out.println("Zu groß!");
+            } else {
+                System.out.println("Richtig! Omedeto!");
+            }
+        }
+    }
+}
